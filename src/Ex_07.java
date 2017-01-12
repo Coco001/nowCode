d9187a33606977dc020dbaa9ba1690741deb07c6
Coco001 <wsgkk000@163.com>
2017-01-12 14:08:19 +0800

    排序之后相邻两数值差最大

--- src/Ex_07.java
+++ src/Ex_07.java
@@ -1,7 +1,8 @@
 /**
- * 排序之后相邻两数只差最大
+ * 排序之后相邻两数值差最大
  */
 public class Ex_07 {
+    
     private static int maxGap(int[] A, int n) {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
@@ -9,13 +10,16 @@ public class Ex_07 {
             min = Math.min(min, A[i]);
             max = Math.max(max, A[i]);
         }
+        
         if (min == max) {
             return 0;
         }
+        
         boolean[] hasNum = new boolean[n + 1];
         int[] maxs = new int[n + 1];
         int[] mins = new int[n + 1];
         int bid = 0;
+        
         for (int i = 0; i < n; i++) {
             bid = bucket(A[i], n, min, max); // 算出A[i]的桶号
             mins[bid] = hasNum[bid] ? Math.min(mins[bid], A[i]) : A[i];//记录桶中最小值
@@ -31,6 +35,7 @@ public class Ex_07 {
                 break;
             }
         }
+        
         for (; i <= n; i++) {
             if (hasNum[i]) {// 桶非空
                 res = Math.max(res, mins[i] - lastMax);//求取当前桶中的最小值与上一个桶中的最大值的差，对比求取差值最大的
