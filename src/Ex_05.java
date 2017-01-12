55f568f3c25c2f3b18daa9014ebcce53543892ba
Coco001 <wsgkk000@163.com>
2017-01-12 14:05:18 +0800

    查找矩阵中是否包含某一个元素

--- src/Ex_05.java
+++ src/Ex_05.java
@@ -2,6 +2,7 @@
  * 查找矩阵中是否包含某一个元素
  */
 public class Ex_05 {
+    
     public static boolean findX(int[][] A, int n, int m, int x) {
         int r = 0, c = m - 1;
         while (r < n && c >= 0) {
