d28b2a03d5994fbb9701baba70ef59afbd682607
Coco001 <wsgkk000@163.com>
2017-01-12 14:03:24 +0800

    三色排序问题

--- src/Ex_04.java
+++ src/Ex_04.java
@@ -3,11 +3,11 @@
  * 有一个只由 0 1 2三种元素组成的数组，使用交换、原地排序进行排序。
  */
 public class Ex_04 {
-
     public static void main(String[] args) {
         int[] test = new int[]{1, 1, 2, 2, 1, 1, 0, 0, 1, 0, 2, 0};
         sortThreeColor(test, 12);
     }
+    
     private static int[] sortThreeColor(int[] A, int n) {
         int left = -1;//记录0元素个数索引
         int index = 0;//
